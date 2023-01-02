890e668e53366ea4ef28887b5773561d6d9b9182
Dev-001 <Dev-001@Forges-MacBook-Pro.local>
2023-01-02 12:49:06 -0800

    Task: Project Configuration, Desc: Full project configurations and setups, Status: Completed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,25 @@
 
 namespace App\Exceptions;
 
+use App\Traits\apiResponseBuilder;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\RelationNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use apiResponseBuilder;
+
     /**
      * A list of exception types with their corresponding custom log levels.
      *
@@ -30,18 +44,70 @@ class Handler extends ExceptionHandler
      *
      * @var array<int, string>
      */
-    protected $dontFlash = [
+    protected $dontFlash =
+    [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
     /**
+     * @param Request $request
+     * @param Throwable $exception
+     * @return JsonResponse|Response
+     * @throws Throwable
+     */
+    public function render( $request, $exception ) : JsonResponse|Response
+    {
+        if ( $exception instanceof ModelNotFoundException )
+        {
+            return $this -> errorResponse( array(), 'Error', 'Resource not found', Response::HTTP_NOT_FOUND );
+        }
+        elseif ( $exception instanceof MethodNotAllowedHttpException )
+        {
+            return $this -> errorResponse( array(), 'Error', 'You do not have permission to perform this action', Response::HTTP_METHOD_NOT_ALLOWED );
+        }
+        elseif ( $exception instanceof NotFoundHttpException )
+        {
+            return $this -> errorResponse( array(), 'Error', 'Resource not found', Response::HTTP_NOT_FOUND );
+        }
+        elseif ( $exception instanceof QueryException )
+        {
+            return $this -> errorResponse( array(), 'Error', 'Connection refused', Response::HTTP_UNAUTHORIZED );
+        }
+        elseif ( $exception instanceof RelationNotFoundException )
+        {
+            return $this -> errorResponse( array(), 'Error', 'Undefined relationship', Response::HTTP_INTERNAL_SERVER_ERROR );
+        }
+        elseif ( $exception instanceof AccessDeniedHttpException )
+        {
+            return $this -> errorResponse( array(), 'Error', 'This action is unauthorized.', Response::HTTP_FORBIDDEN );
+        }
+
+        return parent::render( $request, $exception );
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return JsonResponse|RedirectResponse|Response
+     * @throws Throwable
+     */
+    protected function unauthenticated( $request, AuthenticationException $exception ) : JsonResponse|RedirectResponse|Response
+    {
+        if ( $request -> expectsJson() )
+        {
+            return $this -> errorResponse( array(), 'Error', 'User not authenticated', Response::HTTP_UNAUTHORIZED );
+        }
+        return redirect() -> guest('login');
+    }
+
+    /**
      * Register the exception handling callbacks for the application.
      *
      * @return void
      */
-    public function register()
+    public function register() : void
     {
         $this->reportable(function (Throwable $e) {
             //
---
+++ app/Traits/apiResponseBuilder.php
@@ -0,0 +1,31 @@
+<?php
+    namespace App\Traits;
+
+    use Illuminate\Http\JsonResponse;
+
+    trait apiResponseBuilder
+    {
+        /**
+         * @param $data
+         * @param $status_message
+         * @param $message
+         * @param $status_code
+         * @return JsonResponse
+         */
+        public function successResponse( $data, $status_message, $message, $status_code ) : JsonResponse
+        {
+            return response() -> json([ 'status' => $status_message, 'code' => $status_code, 'message' => $message, 'data' => $data ] );
+        }
+
+        /**
+         * @param $data
+         * @param $status_message
+         * @param $message
+         * @param $status_code
+         * @return JsonResponse
+         */
+        public function errorResponse( $data, $status_message, $message, $status_code ) : JsonResponse
+        {
+            return response() -> json([ 'status' => $status_message, 'code' => $status_code, 'message' => $message, 'data' => $data, ] );
+        }
+    }
